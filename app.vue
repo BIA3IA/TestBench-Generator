ad9647bab56369df0e24f61b43f56f4ff6145435
Bianca <ianoselbiancaroberta02@gmail.com>
2025-03-11 18:45:37 +0100

    add Analytics component

--- app.vue
+++ app.vue
@@ -2,9 +2,11 @@
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
+	<Analytics />
 </template>
 
 <script setup>
+import { Analytics } from '@vercel/analytics/nuxt'
 
 const appConfig = useAppConfig();
 useHead({
